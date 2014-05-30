961720194ee43dc35cfd0d8458af7b610f4790c4
Nick Harberg <nick@seamonsterstudios.com>
2014-05-29 23:13:18 -0700

    added styles

--- app/js/config.js
+++ app/js/config.js
@@ -17,6 +17,7 @@
             "propertyParser": "vendor/propertyParser",
             "isotope": "vendor/isotope.pkgd",
             "backbone": "vendor/backbone",
+            "bootstrap": "vendor/bootstrap/js/bootstrap",
             "gmaps": "http://maps.googleapis.com/maps/api/js?v=3.exp?key={AIzaSyAckmSzoxdbOdFhNltb9ufCWuTackzcupc}&sensor=false&libraries=places",
 
             // application libraries
@@ -27,6 +28,7 @@
     });
     require(["scripts"], function(){});
     require(["client"], function(){});
+    require(["bootstrap"], function(){});
     //require(["async!http://maps.googleapis.com/maps/api/js?v=3.exp?key={AIzaSyAckmSzoxdbOdFhNltb9ufCWuTackzcupc}&sensor=false&libraries=places"], function(){});
     //require(['maps'], function(){});
 }());
